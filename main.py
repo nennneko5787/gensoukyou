c0e4b81ef97f1341f9c1fe2f94eb4d2b9ce19bb9
nennneko5787 <nennneko5787@gmail.com>
2024-04-11 19:46:43 +0900

    ふらん！

--- main.py
+++ main.py
@@ -113,6 +113,13 @@ async def initialize(interaction: discord.Interaction):
 			mentionable=True,
 			reason="「幻想郷」ボットの /init コマンドのリクエストにより作成されました。 ※重複してロールが作成されることはありません。"
 		)
+	if discord.utils.get(interaction.guild.roles, name='フランドール・スカーレット') is None:
+		await interaction.guild.create_role(
+			name="フランドール・スカーレット",
+			color=discord.Colour.from_str("#e8b177"),
+			mentionable=True,
+			reason="「幻想郷」ボットの /init コマンドのリクエストにより作成されました。 ※重複してロールが作成されることはありません。"
+		)
 
 	embed = discord.Embed(
 		title="✅初期化に成功しました。",
@@ -129,16 +136,23 @@ async def chat_clean(interaction: discord.Interaction):
 async def on_message(message: discord.Message):
 	if message.type == discord.MessageType.default or message.type == discord.MessageType.reply:
 		if message.author.bot == False:
-			if discord.utils.find(lambda r: r.id == 1226065401650352148, message.mentions):
+			mentioned = False
+			if discord.utils.find(lambda r: r.name == '博麗霊夢', message.role_mentions) or discord.utils.find(lambda e: e.author.name == '霧雨魔理沙', message.embeds):
+				await asyncio.create_task(博麗霊夢(message))
+				mentioned = True
+			if discord.utils.find(lambda r: r.name == '霧雨魔理沙', message.role_mentions) or discord.utils.find(lambda e: e.author.name == '霧雨魔理沙', message.embeds):
+				await asyncio.create_task(霧雨魔理沙(message))
+				mentioned = True
+			if discord.utils.find(lambda r: r.name == 'フランドール・スカーレット', message.role_mentions) or discord.utils.find(lambda e: e.author.name == 'フランドール・スカーレット', message.embeds):
+				await asyncio.create_task(フランドール＿スカーレット(message))
+				mentioned = True
+
+			if mentioned == False:
 				embed = discord.Embed(
 					title="このボットの使い方",
 					description=f"{discord.utils.find(lambda r: r.name == '博麗霊夢', message.guild.roles).mention} や {discord.utils.find(lambda r: r.name == '霧雨魔理沙', message.guild.roles).mention} にメンションするだけ。"
 				)
 				await asyncio.create_task(message.channel.send(embed=embed))
-			if discord.utils.find(lambda r: r.name == '博麗霊夢', message.role_mentions):
-				await asyncio.create_task(博麗霊夢(message))
-			if discord.utils.find(lambda r: r.name == '霧雨魔理沙', message.role_mentions):
-				await asyncio.create_task(霧雨魔理沙(message))
 
 async def 博麗霊夢(message: discord.Message):
 	prompt = "あなたは、博麗霊夢です。"\
@@ -187,6 +201,31 @@ async def 霧雨魔理沙(message: discord.Message):
 			traceback_info = traceback.format_exc()
 			text = f"どうやら霧雨魔理沙の機嫌が悪いらしい...\n```\n{traceback_info}\n```"
 			embed = discord.Embed(description=text,color=discord.Colour.from_str("#d8ce17")).set_author(name="霧雨魔理沙", icon_url="https://s3.ap-northeast-1.amazonaws.com/duno.jp/icons/th000-000201.png")
+			await message.reply(text)
+	return
+
+async def フランドール＿スカーレット(message: discord.Message):
+	prompt = "あなたは、フランドール・スカーレットです。"\
+			f"私の名前は{message.author.display_name}です。私はあなたに「{message.clean_content}」と話しました。あなたはフランドール・スカーレットなので、フランドール・スカーレットのように出力してください。人と話すときと同じように出力してください。文法的に誤りのある文は認められません。"\
+			"返答にはMarkdown記法を使うことができます。"
+#	if message.type == discord.MessageType.reply:
+#		prompt = f"{prompt}また、私は、{message.reference.cached_message.author.display_name}さんの「{message.reference.cached_message.clean_content}」というメッセージに返信しています。"
+
+	if chat_rooms[message.author.id] == None:
+		# チャットを開始
+		chat_rooms[message.author.id] = model.start_chat(history=[])
+
+	async with message.channel.typing():
+		try:
+			# Gemini APIを使って応答を生成 (非同期で実行)
+			response = await asyncio.to_thread(chat_rooms[message.author.id].send_message, prompt)
+
+			embed = discord.Embed(title="",description=response.text,color=discord.Colour.from_str("#e8b177")).set_author(name="フランドール・スカーレット", icon_url="https://s3.ap-northeast-1.amazonaws.com/duno.jp/icons/th060-070101.png")
+			await message.reply(embed=embed)
+		except:
+			traceback_info = traceback.format_exc()
+			text = f"どうやらフランドール・スカーレットの機嫌が悪いらしい...\n```\n{traceback_info}\n```"
+			embed = discord.Embed(description=text,color=discord.Colour.from_str("#e8b177")).set_author(name="フランドール・スカーレット", icon_url="https://s3.ap-northeast-1.amazonaws.com/duno.jp/icons/th060-070101.png")
 			await message.reply(text)
 	return
 
