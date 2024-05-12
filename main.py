3b1df2f1404b867bc7694f46f497e8fe02081ad3
ねんねこ㌨ <nennneko5787@gmail.com>
2024-05-13 06:02:26 +0900

    ねおちしちゃったんよね

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import discord
 from discord import app_commands
 from discord.ext import tasks
 import os
-import google.generativeai as genai
+import openai
 from collections import defaultdict
 import random
 import asyncio
@@ -11,43 +11,15 @@ from keep_alive import keep_alive
 import signal
 import sys
 
-chat_rooms = defaultdict(lambda: None)
+chat_rooms = defaultdict(list)
 
 if os.path.isfile(".env"):
 	from dotenv import load_dotenv
 	load_dotenv(verbose=True)
 
-genai.configure(api_key=os.getenv("gemini"))
-# Set up the model
-generation_config = {
-	"temperature": 0.9,
-	"top_p": 1,
-	"top_k": 1,
-	"max_output_tokens": 2000,
-}
+openai.api_key = os.getenv("openai")
+openai.base_url = "https://api.pawan.krd/v1/"
 
-safety_settings = [
-{
-	"category": "HARM_CATEGORY_HARASSMENT",
-	"threshold": "BLOCK_NONE"
-},
-{
-	"category": "HARM_CATEGORY_HATE_SPEECH",
-	"threshold": "BLOCK_NONE"
-},
-{
-	"category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
-	"threshold": "BLOCK_NONE"
-},
-{
-	"category": "HARM_CATEGORY_DANGEROUS_CONTENT",
-	"threshold": "BLOCK_NONE"
-},
-]
-
-model = genai.GenerativeModel(model_name="gemini-1.5-pro-latest",
-							generation_config=generation_config,
-							safety_settings=safety_settings)
 
 role_info = {
 	"": {
